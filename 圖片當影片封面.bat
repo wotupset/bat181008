9442a5c9a80e9ea10fa051bb43e7b44c0331347b
断固非難 <4385327+wotupset@users.noreply.github.com>
2020-01-25 01:35:22 +0800

    Add files via upload

--- 圖片當影片封面.bat
+++ 圖片當影片封面.bat
@@ -13,7 +13,7 @@ set oo2=^
 echo %oo2%
 
 
-ffmpeg -y  %oo1% %oo2%  "output.mp4"
+ffmpeg -y  %oo1% %oo2%  "圖片當影片封面.mp4"
 
 
 pause
@@ -23,3 +23,6 @@ exit
 
 -map 0:a -map 1:v 取第一個檔案的音軌 取第二個檔案的影像
 -f lavfi -i anullsrc  虛擬空白音軌
+-vf scale=320:240
+
+-vf scale=iw*2:ih   iw =輸入寬度，ih =輸入高度 
