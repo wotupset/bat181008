8f24c579326397f8f97ea27e9cd9f7f39743c8e5
断固非難 <4385327+wotupset@users.noreply.github.com>
2020-01-25 01:34:18 +0800

    Add files via upload

--- 圖片當影片封面.bat
+++ 圖片當影片封面.bat
@@ -1,3 +1,25 @@
-ffmpeg -y  -i "EAXh7dGUcAAXTJ8_orig.jpg" -i "sf_theme.wav"  output.mp4
+set oo1=^
+-i "01.mp4" ^
+-loop 1 -i "01.jpg" 
 
-cmd
+
+echo %oo1%
+
+set oo2=^
+-shortest ^
+-map 0:a -map 1:v 
+
+
+echo %oo2%
+
+
+ffmpeg -y  %oo1% %oo2%  "output.mp4"
+
+
+pause
+exit
+-t 10
+-shortest
+
+-map 0:a -map 1:v 取第一個檔案的音軌 取第二個檔案的影像
+-f lavfi -i anullsrc  虛擬空白音軌
