11e78c9d9f5e615729132571c2c9dd40a8852b1f
断固非難 <4385327+wotupset@users.noreply.github.com>
2022-03-16 01:51:16 +0800

    Add files via upload

---
+++ youtube-dl/下載youtube_worst.bat
@@ -0,0 +1,43 @@
+@echo off
+
+
+
+:top
+
+
+set /p poi=youtube網址:
+echo %poi%
+
+
+set qq01=-o yt-worst-%%(title)s-%%(id)s.%%(ext)s
+echo %qq01%
+
+
+
+
+yt-dlp %qq01%  -f worstvideo[ext=mp4]+worstaudio[ext=m4a]  %poi%
+
+
+
+goto top
+
+
+
+pause
+exit
+--limit-rate 2M
+
+--cookies=cookiejar.txt
+
+裁剪秒數 但依然要下載完整影片
+youtube-dl --postprocessor-args "-ss 0:9:0 -to 0:9:10" https://www.youtube.com/watch?v=OCmqTRHOLNw
+
+
+
+youtube-dl -f bestvideo[ext=mp4],bestaudio[ext=m4a]   %poi%
+
+
+
+
+-o [%(id)s]_%(title)s.%(ext)s
+youtube-dl -f best -o [%(id)s]_%(title)s.%(ext)s  https://www.youtube.com/watch?v=_dXJK2acUos
