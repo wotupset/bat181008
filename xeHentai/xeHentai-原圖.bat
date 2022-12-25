83b5303c5d4b79e8e7b1e31485aaaa0f9c76edfd
断固非難 <4385327+wotupset@users.noreply.github.com>
2022-12-25 19:01:58 +0800

    Add files via upload

---
+++ xeHentai/xeHentai-原圖.bat
@@ -0,0 +1,27 @@
+echo off
+chcp 65001
+
+:top
+
+
+set /p poi=網址:
+echo %poi%
+
+
+start "" "xeHentai-2.0.2.3.exe" %poi%  -o
+
+
+goto top
+
+
+
+pause
+exit
+
+
+del /f /q *.txt
+
+
+start "" "xeHentai-2.0.1.4.exe" %poi% -t 2 -o -s 1
+
+-u 帳號 -k 密碼
--- xeHentai/xeHentai-自訂範圍.bat
+++ xeHentai/xeHentai-自訂範圍.bat
@@ -2,8 +2,8 @@ echo off
 chcp 65001
 
 
-set url=https://exhentai.org/g/2234704/93f3bdb39c/
-start "" "xeHentai-2.0.2.3.exe" %url% --download-range 39-62
+set url=https://exhentai.org/g/2365444/dfae306562/
+start "" "xeHentai-2.0.2.3.exe" %url% --download-range 87-98
 
 
 
@@ -17,3 +17,4 @@ cmd
 
 xeHentai-2.0.1.9.exe https://exhentai.org/g/1333395/6c60559f65/ -o --download-range 8-55
 
+-u 帳號 -k 密碼
